1a0a2523eb800770342dbf59d6f446d5d8a2a71d
Bavon101 <bavonfate@gmail.com>
2022-02-01 17:42:53 +0300

    Fix: Linting errors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const menuBtn = document.querySelector('.menu_button');
 const closeBtn = document.querySelector('.close_button');
 function openCloseDrawer() {
-	const menuDrawer = document.querySelector('.open_nav');
-	menuDrawer.classList.toggle('show_drawer');
-	menuBtn.classList.toggle('show_drawer');
-	closeBtn.classList.toggle('show_drawer');
-
+  const menuDrawer = document.querySelector('.open_nav');
+  menuDrawer.classList.toggle('show_drawer');
+  menuBtn.classList.toggle('show_drawer');
+  closeBtn.classList.toggle('show_drawer');
 }
 
 menuBtn.addEventListener('click', openCloseDrawer);
